bafd85f630cabd57dcde96570d568221a3a4c9c0
Yada dev <yadadev.io@gmail.com>
2022-07-14 10:55:24 -0400

    connect wallet with web3uikit

---
+++ components/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <h1 className="text-6xl font-bold text-blue-600 mb-6">Buy Me A Tea</h1>
+    </main>
+  );
+};
+
+export default MainPage;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
+import MainPage from "../components/MainPage";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Mini Tea Shop - Buy my tea</title>
         <meta name="description" content="my tea shop" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <MainPage />
     </div>
   );
 }
